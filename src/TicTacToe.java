09d1cd587cab4c91415d11067738fdacc0d255e5
Vladimir Mityukov <mityukov@gmail.com>
2012-10-16 17:03:28 +0400

    Adv. exercise #1: Use 2D arrays instead of plain ones

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
 
 public class TicTacToe implements ActionListener {
 
-    JButton squares[];
+    JButton squares[][];
     JButton newGameButton;
     JLabel score;
     JLabel winsCounter;
@@ -68,16 +68,18 @@ public class TicTacToe implements ActionListener {
         windowContent.add(score, "South");
 
         // массив из 9 кнопок
-        squares = new JButton[9];
+        squares = new JButton[3][3];
 
         // создаем кнопки
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
 
-            squares[i] = new JButton();
-            squares[i].addActionListener(this);
-            squares[i].setBackground(Color.ORANGE);
-            squares[i].setText(" "); // fix too narrow buttons
-            centerPanel.add(squares[i]);
+            squares[i][j] = new JButton();
+            squares[i][j].addActionListener(this);
+            squares[i][j].setBackground(Color.ORANGE);
+            squares[i][j].setText(" "); // fix too narrow buttons
+            centerPanel.add(squares[i][j]);
+        	}
         }
         
         JFrame frame = new JFrame();
@@ -85,8 +87,10 @@ public class TicTacToe implements ActionListener {
         frame.pack();
         frame.setVisible(true);
         // fix too narrow buttons, step 2;
-        for (int i=0; i<9; i++) {
-            squares[i].setText("");
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+                squares[i][j].setText("");
+        	}
         }
     }
 
@@ -101,11 +105,13 @@ public class TicTacToe implements ActionListener {
         // нажата кнопка <New Game!>?
         if (theButton == newGameButton) {
 
-            for (int i=0; i<9; i++) {
+        	for (int i=0; i<3; i++) {
+            	for (int j=0; j<3; j++) {
 
-                squares[i].setEnabled(true);
-                squares[i].setText("");
-                squares[i].setBackground(Color.ORANGE);
+                    squares[i][j].setEnabled(true);
+                    squares[i][j].setText("");
+                    squares[i][j].setBackground(Color.ORANGE);
+            	}
             }
 
             emptySquaresLeft = 9;
@@ -118,33 +124,35 @@ public class TicTacToe implements ActionListener {
         String winner = "";
 
         // нажата одна из клеток?
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
 
-            if (theButton == squares[i]) {
-
-            	if (squares[i].getText() == "X" || squares[i].getText() == "O")
-            		break;
-            		
-                squares[i].setText("X");
-                winner = lookForWinner();
-
-                if (!"".equals(winner)) {
-
-                    endTheGame();
-                }
-                else {
-
-                    computerMove();
-                    winner = lookForWinner();
-
-                    if (!"".equals(winner)) {
-
-                        endTheGame();
-                    }
-                }
-
-                break;
-            }
+	            if (theButton == squares[i][j]) {
+	
+	            	if (squares[i][j].getText() == "X" || squares[i][j].getText() == "O")
+	            		break;
+	            		
+	                squares[i][j].setText("X");
+	                winner = lookForWinner();
+	
+	                if (!"".equals(winner)) {
+	
+	                    endTheGame();
+	                }
+	                else {
+	
+	                    computerMove();
+	                    winner = lookForWinner();
+	
+	                    if (!"".equals(winner)) {
+	
+	                        endTheGame();
+	                    }
+	                }
+	
+	                break;
+	            }
+        	}
         } // end of for (...)
 
         if (winner.equals("X")) {
@@ -178,69 +186,69 @@ public class TicTacToe implements ActionListener {
     		return "T"; // ничья (tie)
     	}
     	
-    	// Ряд 1 (элементы: 0,1,2)
-    	if (!squares[0].getText().equals("")
-    		&& squares[0].getText().equals(squares[1].getText())
-    		&& squares[0].getText().equals(squares[2].getText())) {
+    	// Ряд 1 (элементы: 0:0,0:1,0:2)
+    	if (!squares[0][0].getText().equals("")
+    		&& squares[0][0].getText().equals(squares[0][1].getText())
+    		&& squares[0][0].getText().equals(squares[0][2].getText())) {
     		
-    		theWinner = squares[0].getText();
-    		highlightWinner(0,1,2);
+    		theWinner = squares[0][0].getText();
+    		highlightWinner(0,0,0,1,0,2);
     	
-    	// Ряд 2 (элементы: 3,4,5)
-    	} else if (!squares[3].getText().equals("")
-       		&& squares[3].getText().equals(squares[4].getText())
-       		&& squares[3].getText().equals(squares[5].getText())) {
+    	// Ряд 2 (элементы: 1:0,1:1,1:2)
+    	} else if (!squares[1][0].getText().equals("")
+       		&& squares[1][0].getText().equals(squares[1][1].getText())
+       		&& squares[1][0].getText().equals(squares[1][2].getText())) {
      
-    		theWinner = squares[3].getText();
-    		highlightWinner(3,4,5);
+    		theWinner = squares[1][0].getText();
+    		highlightWinner(1,0,1,1,1,2);
     		
-    	// Ряд 3 (элементы: 6,7,8)
-    	} else if (!squares[6].getText().equals("")
-         	&& squares[6].getText().equals(squares[7].getText())
-           	&& squares[6].getText().equals(squares[8].getText())) {
+    	// Ряд 3 (элементы: 2:0,2:1,2:2)
+    	} else if (!squares[2][0].getText().equals("")
+         	&& squares[2][0].getText().equals(squares[2][1].getText())
+           	&& squares[2][0].getText().equals(squares[2][2].getText())) {
          
-        	theWinner = squares[6].getText();
-        	highlightWinner(6,7,8);
+        	theWinner = squares[2][0].getText();
+        	highlightWinner(2,0,2,1,2,2);
         	
-        // Колонка 1 (элементы: 0,3,6)
-        } else if (!squares[0].getText().equals("")
-            && squares[0].getText().equals(squares[3].getText())
-            && squares[0].getText().equals(squares[6].getText())) {
+        // Колонка 1 (элементы: 0:0,1:0,2:0)
+        } else if (!squares[0][0].getText().equals("")
+            && squares[0][0].getText().equals(squares[1][0].getText())
+            && squares[0][0].getText().equals(squares[2][0].getText())) {
              
-            theWinner = squares[0].getText();
-            highlightWinner(0,3,6);
+            theWinner = squares[0][0].getText();
+            highlightWinner(0,0,1,0,2,0);
 
-        // Колонка 2 (элементы: 1,4,7)
-        } else if (!squares[1].getText().equals("")
-            && squares[1].getText().equals(squares[4].getText())
-            && squares[1].getText().equals(squares[7].getText())) {
+        // Колонка 2 (элементы: 0:1,1:1,2:1)
+        } else if (!squares[0][1].getText().equals("")
+            && squares[0][1].getText().equals(squares[1][1].getText())
+            && squares[0][1].getText().equals(squares[2][1].getText())) {
                  
-            theWinner = squares[1].getText();
-            highlightWinner(1,4,7);
+            theWinner = squares[0][1].getText();
+            highlightWinner(0,1,1,1,2,1);
 
-        // Колонка 3 (элементы: 2,5,8)
-        } else if (!squares[2].getText().equals("")
-            && squares[2].getText().equals(squares[5].getText())
-            && squares[2].getText().equals(squares[8].getText())) {
+        // Колонка 3 (элементы: 0:2,1:2,2:2)
+        } else if (!squares[0][2].getText().equals("")
+            && squares[0][2].getText().equals(squares[1][2].getText())
+            && squares[0][2].getText().equals(squares[2][2].getText())) {
                  
-            theWinner = squares[2].getText();
-            highlightWinner(2,5,8);
+            theWinner = squares[0][2].getText();
+            highlightWinner(0,2,1,2,2,2);
         
-        // Диагональ 1 (элементы: 0,4,8)
-        } else if (!squares[0].getText().equals("")
-            && squares[0].getText().equals(squares[4].getText())
-            && squares[0].getText().equals(squares[8].getText())) {
+        // Диагональ 1 (элементы: 0:0,1:1,2:2)
+        } else if (!squares[0][0].getText().equals("")
+            && squares[0][0].getText().equals(squares[1][1].getText())
+            && squares[0][0].getText().equals(squares[2][2].getText())) {
                          
-            theWinner = squares[0].getText();
-            highlightWinner(0,4,8);
+            theWinner = squares[0][0].getText();
+            highlightWinner(0,0,1,1,2,2);
 
-        // Диагональ 2 (элементы: 2,4,6)
-        } else if (!squares[2].getText().equals("")
-            && squares[2].getText().equals(squares[4].getText())
-            && squares[2].getText().equals(squares[6].getText())) {
+        // Диагональ 2 (элементы: 0:2,1;1,2:0)
+        } else if (!squares[0][2].getText().equals("")
+            && squares[0][2].getText().equals(squares[1][1].getText())
+            && squares[0][2].getText().equals(squares[2][0].getText())) {
                          
-            theWinner = squares[2].getText();
-            highlightWinner(2,4,6);
+            theWinner = squares[0][2].getText();
+            highlightWinner(0,2,1,1,2,0);
         }
     	
     	return theWinner;
@@ -251,29 +259,30 @@ public class TicTacToe implements ActionListener {
      */
     void computerMove() {
     	
-    	int selectedSquare;
+    	int selectedSquare[];
     	
     	// сначала ищем строку где нехватает нолика
     	selectedSquare = findEmptySquare("O");
     	
     	// если нет, то пытаемся не дать оппоненту поставить крестик
-    	if (selectedSquare == -1) {
+    	if (selectedSquare[0] == -1) {
     		selectedSquare = findEmptySquare("X");
     	}
     	
     	// иначе, пытаемся занять центральную клетку
-    	if ( (selectedSquare == -1)
-    			&& (squares[4].getText().equals("")) ) {
+    	if ( (selectedSquare[0] == -1)
+    			&& (squares[1][1].getText().equals("")) ) {
     		
-    		selectedSquare = 4;
+    		selectedSquare[0] = 1; selectedSquare[1] = 1;
     	}
     	
     	// иначе просто занимаем рандомную свободную
-    	if (selectedSquare == -1) {
+    	if (selectedSquare[0] == -1) {
     		selectedSquare = getRandomSquare();
     	}
     	
-    	squares[selectedSquare].setText("O");    	
+    	
+    	squares[selectedSquare[0]][selectedSquare[1]].setText("O");    	
     }
     
     /**
@@ -281,145 +290,170 @@ public class TicTacToe implements ActionListener {
      * чтобы узнать, есть ли там две клетки с одинаковыми
      * надписями и пустой клеткой
      * @param		player - "X" или "O"
-     * @return		номер пустой клетки
-     * 				или -1 если ничего не найдено
+     * @return		массив с номером пустой клетки {i,j}
+     * 				или -1 в первом элементе если ничего не найдено
      */
-    int findEmptySquare(String player) {
+    int [] findEmptySquare(String player) {
 
-    	int weight[] = new int[9];
+    	int weight[][] = new int[3][3];
+    	int res[] = new int[2];
 
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
 
-            if (squares[i].getText().equals("O")) {
-                weight[i] = -1;
-            } else if (squares[i].getText().equals("X")) {
-                weight[i] = 1;
-            } else {
-                weight[i] = 0;
-            }
+                if (squares[i][j].getText().equals("O")) {
+                    weight[i][j] = -1;
+                } else if (squares[i][j].getText().equals("X")) {
+                    weight[i][j] = 1;
+                } else {
+                    weight[i][j] = 0;
+                }
+        	}
         }
 
         int twoWeights = player.equals("O") ? -2 : 2;
 
         // есть ли в ряду один две одинаковые клетки и одна пустая
-        if (weight[0] + weight[1] + weight[2] == twoWeights) {
+        if (weight[0][0] + weight[0][1] + weight[0][2] == twoWeights) {
 
-            if (weight[0] == 0)
-                return 0;
-            else if (weight[1] == 0)
-                return 1;
-            else
-                return 2;
+            if (weight[0][0] == 0) {
+            	res[0]=0;res[1]=0;
+            } else if (weight[0][1] == 0) {
+                res[0]=0;res[1]=1;
+            } else {
+                res[0]=0;res[1]=2;
+            }        
+            return res;
         }
+        
         // есть ли в ряду два две одинаковые клетки и одна пустая
-        if (weight[3] + weight[4] + weight[5] == twoWeights) {
+        if (weight[1][0] + weight[1][1] + weight[1][2] == twoWeights) {
 
-            if (weight[3] == 0)
-                return 3;
-            else if (weight[4] == 0)
-                return 4;
-            else
-                return 5;
+            if (weight[1][0] == 0) {
+                res[0]=1;res[1]=0;
+            } else if (weight[1][1] == 0) {
+                res[0]=1;res[1]=1;
+            } else {
+                res[0]=1;res[1]=2;
+            }
+            return res;
         }
+        
         // есть ли в ряду три две одинаковые клетки и одна пустая
-        if (weight[6] + weight[7] + weight[8] == twoWeights) {
+        if (weight[2][0] + weight[2][1] + weight[2][2] == twoWeights) {
 
-            if (weight[6] == 0)
-                return 6;
-            else if (weight[7] == 0)
-                return 7;
-            else
-                return 8;
+        	if (weight[2][0] == 0) {
+                res[0]=2;res[1]=0;
+            } else if (weight[2][1] == 0) {
+                res[0]=2;res[1]=1;
+            } else {
+                res[0]=2;res[1]=2;
+            }
+            return res;
         }
 
         // есть ли в колонке один две одинаковые клетки и одна пустая
-        if (weight[0] + weight[3] + weight[6] == twoWeights) {
+        if (weight[0][0] + weight[1][0] + weight[2][0] == twoWeights) {
 
-            if (weight[0] == 0)
-                return 0;
-            else if (weight[3] == 0)
-                return 3;
-            else
-                return 6;
+        	if (weight[0][0] == 0) {
+                res[0]=0;res[1]=0;
+            } else if (weight[1][0] == 0) {
+                res[0]=1;res[1]=0;
+            } else {
+                res[0]=2;res[1]=0;
+            }
+            return res;
         }
         // есть ли в колонке два две одинаковые клетки и одна пустая
-        if (weight[1] + weight[4] + weight[7] == twoWeights) {
+        if (weight[0][1] + weight[1][1] + weight[2][1] == twoWeights) {
 
-            if (weight[1] == 0)
-                return 1;
-            else if (weight[4] == 0)
-                return 4;
-            else
-                return 7;
+        	if (weight[0][1] == 0) {
+                res[0]=0;res[1]=1;
+            } else if (weight[1][1] == 0) {
+                res[0]=1;res[1]=1;
+            } else {
+                res[0]=2;res[1]=1;
+            }
+            return res;
         }
         // есть ли в колонке три две одинаковые клетки и одна пустая
-        if (weight[2] + weight[5] + weight[8] == twoWeights) {
+        if (weight[0][2] + weight[1][2] + weight[2][2] == twoWeights) {
 
-            if (weight[2] == 0)
-                return 2;
-            else if (weight[5] == 0)
-                return 5;
-            else
-                return 8;
+        	if (weight[0][2] == 0) {
+                res[0]=0;res[1]=2;
+            } else if (weight[1][2] == 0) {
+                res[0]=1;res[1]=2;
+            } else {
+                res[0]=2;res[1]=2;
+            }
+            return res;
         }
 
         // есть ли в первой диагонали две одинаковые клетки и одна пустая
-        if (weight[0] + weight[4] + weight[8] == twoWeights) {
+        if (weight[0][0] + weight[1][1] + weight[2][2] == twoWeights) {
 
-            if (weight[0] == 0)
-                return 0;
-            else if (weight[4] == 0)
-                return 4;
-            else
-                return 8;
+        	if (weight[0][0] == 0) {
+                res[0]=0;res[1]=0;
+            } else if (weight[1][1] == 0) {
+                res[0]=1;res[1]=1;
+            } else {
+                res[0]=2;res[1]=2;
+            }
+            return res;
         }
         // есть ли во второй диагонали две одинаковые клетки и одна пустая
-        if (weight[2] + weight[4] + weight[6] == twoWeights) {
+        if (weight[0][2] + weight[1][1] + weight[2][0] == twoWeights) {
 
-            if (weight[2] == 0)
-                return 2;
-            else if (weight[4] == 0)
-                return 4;
-            else
-                return 6;
+        	if (weight[0][2] == 0) {
+                res[0]=0;res[1]=2;
+            } else if (weight[1][1] == 0) {
+                res[0]=1;res[1]=1;
+            } else {
+                res[0]=2;res[1]=0;
+            }
+            return res;
         }
         
         // не найдено двух одинаковых соседних клеток
-        return -1;
+        res[0] = -1; res[1] = -1;
+        return res;
     }
 
-    int getRandomSquare() {
+    int [] getRandomSquare() {
 
         boolean gotEmptySquare = false;
 
-        int selectedSquare = -1;
+        int res[] = new int[2];
 
         do {
 
-            selectedSquare = (int) (Math.random() * 9);
+            res[0] = (int) (Math.random() * 3);
+            res[1] = (int) (Math.random() * 3);
 
-            if (squares[selectedSquare].getText().equals("")) {
+            if (squares[res[0]][res[1]].getText().equals("")) {
                 gotEmptySquare = true; // чтобы закончить цикл
             }
         } while (!gotEmptySquare);
 
-        return selectedSquare;
+        return res;
     }
 
-    void highlightWinner(int win1, int win2, int win3) {
+    void highlightWinner(int win1i, int win1j, int win2i, int win2j, int win3i, int win3j) {
 
-        squares[win1].setBackground(Color.CYAN);
-        squares[win2].setBackground(Color.CYAN);
-        squares[win3].setBackground(Color.CYAN);
+        squares[win1i][win1j].setBackground(Color.CYAN);
+        squares[win2i][win2j].setBackground(Color.CYAN);
+        squares[win3i][win3j].setBackground(Color.CYAN);
     }
 
     void endTheGame() {
 
         newGameButton.setEnabled(true);
 
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
 
-            squares[i].setEnabled(false);
+                squares[i][j].setEnabled(false);
+        	}
         }
     }
     
